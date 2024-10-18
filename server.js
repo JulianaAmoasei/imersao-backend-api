7db6c17f306dcfb29b1fa2ca94f52e8970561e37
JulianaAmoasei <late.parrot@gmail.com>
2024-10-18 17:53:31 -0300

    add: bloco 1 - criando local server

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+const posts = [
+  {
+    id: 1,
+    imgUrl: "https://placecats.com/millie/300/150",
+    descricao: "Fotografia de um gato adulto rajado olhando para a câmera",
+  },
+  {
+    id: 2,
+    imgUrl: "https://placecats.com/millie/300/150",
+    descricao: "Fotografia de um gato adulto rajado olhando para a câmera",
+  },
+  {
+    id: 3,
+    imgUrl: "https://placecats.com/millie/300/150",
+    descricao: "Fotografia de um gato adulto rajado olhando para a câmera",
+  }
+]
+
+app.get("/", (req, res) => {
+  res.status(200).json(posts);
+});
+
+app.listen(PORT, () => {
+  console.log("servidor escutando!");
+});
