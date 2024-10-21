36b88c9438089404c8cb411abd1ae44430a4a449
JulianaAmoasei <late.parrot@gmail.com>
2024-10-21 16:20:10 -0300

    add: bloco 3 - opção testar get sem rotas

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from "express";
 import conectarAoBanco from "./src/config/dbConfig.js";
+import routes from "./src/routes/postsRoute.js";
 
-const app = express();
 const PORT = 3000;
+const app = express();
+routes(app);
 
-app.use(express.json());
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-await conectarAoBanco(process.env.STRING_CONEXAO);
+async function getTodosPosts() {
+  const db = conexao.db('appfotos');
+  const colecao = db.collection('posts');
+  return colecao.find().toArray();
+}
+
+const resultado = await getTodosPosts();
+console.log(resultado);
 
 app.listen(PORT, () => {
   console.log("servidor escutando!");
