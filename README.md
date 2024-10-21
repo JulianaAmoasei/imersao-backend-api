3c81dfef2b1c55f03fcc738a1d735f3b29e87dec
JulianaAmoasei <late.parrot@gmail.com>
2024-10-21 15:39:36 -0300

    add bloco 2 - conexão API com banco

---
+++ README.md
@@ -0,0 +1,16 @@
+
+## Aula 1
+
+- Criação da API com endpoint simples
+- dados em memória (array)
+- primeira rota GET sem separação de camadas
+- chave de API do Gemini
+
+## Aula 2 (COM MONGO)
+
+- criação da instância do Mongo Atlas (processo com e sem Google Cloud é basicamente o mesmo)
+- criação do banco, coleção e documentos seed direto no console do Atlas
+- string de conexão e .env
+- lógica de conexão da API com o banco
+- separação do projeto em camadas
+- 
--- server.js
+++ server.js
@@ -1,44 +1,12 @@
 import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
 
 const app = express();
 const PORT = 3000;
 
-console.log(process.env.STRING_CONEXAO);
-
 app.use(express.json());
 
-function buscaPost(id) {
-  return posts.findIndex(post => {
-    return post.id === Number(id);
-  })
-}
-
-const posts = [
-  {
-    id: 1,
-    imgUrl: "https://placecats.com/millie/300/150",
-    descricao: "Fotografia de um gato adulto rajado olhando para a câmera",
-  },
-  {
-    id: 2,
-    imgUrl: "https://placecats.com/millie/300/150",
-    descricao: "Fotografia de um gato adulto rajado olhando para a câmera",
-  },
-  {
-    id: 3,
-    imgUrl: "https://placecats.com/millie/300/150",
-    descricao: "Fotografia de um gato adulto rajado olhando para a câmera",
-  }
-]
-
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
-});
-
-app.get("/posts/:id", (req, res) => {
-  const index = buscaPost(req.params.id);
-  res.status(200).json(posts[index]);
-})
+await conectarAoBanco(process.env.STRING_CONEXAO);
 
 app.listen(PORT, () => {
   console.log("servidor escutando!");
